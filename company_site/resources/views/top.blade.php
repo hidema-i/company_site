787cc3f54252fa501473a94c09e4407c7f3e1903
ShuumaIwamoto <iwa06ki12da@gmail.com>
2021-05-24 21:50:07 +0900

    company-site作成1

---
+++ company_site/resources/views/top.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NIKE Site</title>
+</head>
+
+<body>
+    <header>
+        <h1>NIKE Site</h1>
+        <nav>
+            <a href="/">TOP</a>
+        </nav>
+    </header>
+    <main>
+        <h1>これはTOPページです</h1>
+    </main>
+    <footer>
+        &copy; 2021 NIKE
+    </footer>
+</body>
+
+</html>
